943ca156e458b438cf8712ad2ea40e904258c8f2
Jesus Milano <jesusmilano96@gmail.com>
2025-05-07 21:41:43 -0500

    Add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import AuthProvider from '@/components/AuthProvider';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Snapify',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
+        <Analytics />
         <AuthProvider>
           <main>
             {children}
